1245d2ef15eb5e43597b13c9b18794bbdf846a38
stuuuuuuupid <qwerzxcv11345@gmail.com>
2025-06-09 18:36:13 +0900

    첫 커밋

---
+++ c/test.c
@@ -0,0 +1,9 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main() {
+    SetConsoleOutputCP(65001);
+    printf("안녕하세요!\n");
+
+    return 0;
+}
---
+++ java/mine.java
@@ -0,0 +1,6 @@
+package java;
+public class mine {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("java.version"));
+    }
+}
